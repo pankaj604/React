b7dc8f326836c32a162d3671dd9c98da86c906df
pankaj604 <106951224+pankaj604@users.noreply.github.com>
2023-01-16 03:14:15 +0530

    Create header.jsx

---
+++ MovieReview/header.jsx
@@ -0,0 +1,15 @@
+
+import "./header.css"
+const Header = () => {
+
+    return (
+        <div>
+         <header className="head">
+        <h1 className="h1">Movie review</h1>
+      </header>
+      
+        </div>
+    )
+}
+
+export default Header; 
